28a036033e418c3d7705b30d4fd08d4db6f89090
Bayu Firmawan Paoh #ganteng <bayupaoh@gmail.com>
2016-01-28 15:58:32 +0700

    updatte koneksi.php

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -3,14 +3,8 @@ $host = "ap-cdbr-azure-southeast-a.cloudapp.ne";
 $user = "bda82cf9d10c44";
 $pass = "da2efa4e";
 $database = "rpl_broto";
-//$koneksi = mysql_connect($host,$user,$pass);
-//mysql_select_db($database)
-//or die ("Connection failed".mysql_error());
-try{
-    $conn = new PDO( "mysql:host=$host;dbname=$database", $user, $pass);
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch(Exception $e){
-    die(print_r($e));
-}
+$koneksi = mysql_connect($host,$user,$pass);
+mysql_select_db($database)
+or die ("Connection failed : ".mysql_error());
+
 ?>
